30fcf39234ce5a0462ad7bef67eef93e6d13e608
Travinovs <aleksandrs.travinovs@gmail.com>
2023-04-26 14:25:50 +0300

    Uztaisita jauna aina, iespeja parslegties starp ainam, izveidoti dazi teksta un ievades lauki

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void Begin()
+    {
+        SceneManager.LoadScene("start");
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+
+    public void Stop()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+
+    }
+}
