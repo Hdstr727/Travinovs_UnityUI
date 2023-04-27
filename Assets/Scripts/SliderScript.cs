6bdc30fcd7a5fdf539f7fd840f8b139c78b29713
Travinovs <aleksandrs.travinovs@gmail.com>
2023-04-27 14:40:23 +0300

    Izveidoti 2 slaideri un skripts ar kuru palidzibu var mainit varona garumu un platumu

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour {
+
+    public GameObject HeightSlider;
+    public GameObject WidthSlider;
+    public GameObject Character;
+
+    public void ChangeHeight()
+    {
+        float CurrentHeight = HeightSlider.GetComponent<Slider>().value;
+        Character.transform.localScale = new Vector2(1, 1f * CurrentHeight);
+    }
+
+    public void ChangeWidth()
+    {
+        float CurrentWidth = WidthSlider.GetComponent<Slider>().value;
+        Character.transform.localScale = new Vector2(1f * CurrentWidth, 1);
+    }
+}
