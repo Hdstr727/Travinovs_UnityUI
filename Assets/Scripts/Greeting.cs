c4bbbdf0fc11e603c42e55caf890da6968a271cc
Travinovs <aleksandrs.travinovs@gmail.com>
2023-04-27 14:12:03 +0300

    Uzstaisita poga sveiciens, un skripts ar kuru palidzibu varonis var sasveicinaties ar lietotaju

---
+++ Assets/Scripts/Greeting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+public class Greeting : MonoBehaviour {
+
+	private string Name;
+	private string Age;
+
+	public GameObject InputFieldName;
+	public GameObject InputFieldAge;
+	public GameObject ShowText;
+	public void SaveText()
+	{
+		if (InputFieldName != null && InputFieldAge != null) 
+		{ 
+			Name = InputFieldName.GetComponent<Text>().text;
+            Age = InputFieldAge.GetComponent<Text>().text;
+			string str = "Sveiki! Es esmu "+Name+" man ir "+Age+" gadi!";
+
+            ShowText.GetComponent<Text>().text = str;
+
+        }
+
+		
+	}
+}
