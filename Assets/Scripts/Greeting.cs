e424b626a6530affd33a2f30dae94ded81231749
Hdstr727 <helldestroerr05@gmail.com>
2023-05-07 19:12:33 +0300

    uzlabota ievades lauku darbiba, ka ari noversta iespeja ievadit nekorektus datus

--- Assets/Scripts/Greeting.cs
+++ Assets/Scripts/Greeting.cs
@@ -3,29 +3,55 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Greeting : MonoBehaviour
 {
-
     private string Name;
-    private string Age;
+    private int Age;
 
     public GameObject InputFieldName;
     public GameObject InputFieldAge;
     public GameObject ShowText;
 
+    private void Start()
+    {
+        
+        InputFieldName.GetComponent<InputField>().onValidateInput += ValidateNameInput;
+        InputFieldAge.GetComponent<InputField>().onValidateInput += ValidateAgeInput;
+    }
 
     public void SaveText()
     {
-        InputField InputName = InputFieldName.GetComponent<InputField>();
-        InputField InputAge = InputFieldAge.GetComponent<InputField>();
-        
-        Name = InputFieldName.GetComponent<Text>().text;
-        Age = InputFieldAge.GetComponent<Text>().text;
+        Name = InputFieldName.GetComponent<InputField>().text;
+        Age = int.Parse(InputFieldAge.GetComponent<InputField>().text);
+
         string str = "Sveiki! Es esmu " + Name + " man ir " + Age + " gadi!";
         ShowText.GetComponent<Text>().text = str;
+    }
 
+    private char ValidateNameInput(string text, int charIndex, char addedChar)
+    {
         
+        if (char.IsLetter(addedChar) || addedChar == ' ')
+        {
+            return addedChar;
+        }
+        else
+        {
+            return '\0';
+        }
+    }
 
-
+    private char ValidateAgeInput(string text, int charIndex, char addedChar)
+    {
+        
+        if (char.IsDigit(addedChar))
+        {
+            return addedChar;
+        }
+        else
+        {
+            return '\0';
+        }
     }
 }
